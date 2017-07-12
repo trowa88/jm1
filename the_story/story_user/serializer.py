b5dc95a59dcea0f4e64b813e17341d3909fa5755
trowa88 <trowa020@gmail.com>
2017-07-12 23:53:30 +0900

    1. User 모델 Create 시 비밀번호 암호화 적용 2. requirements.txt 업데이

--- the_story/story_user/serializer.py
+++ the_story/story_user/serializer.py
@@ -5,7 +5,17 @@ from rest_framework import serializers
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = '__all__'
+        fields = ('email', 'username', 'password')
+        extra_kwargs = {'password': {'write_only': True}}
+
+    def create(self, validated_data):
+        user = User(
+            email=validated_data['email'],
+            username=validated_data['username']
+        )
+        user.set_password(validated_data['password'])
+        user.save()
+        return user
 
 
 class GroupSerializer(serializers.ModelSerializer):
