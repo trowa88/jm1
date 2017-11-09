ae02405cefce32d29179558199e9db8ca030d29d
trowa88 <trowa020@gmail.com>
2017-11-09 14:25:00 +0900

    Modify wsgi file

--- the_story/the_story/wsgi.py
+++ the_story/the_story/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "the_story.settings.base")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "the_story.settings")
 
 application = get_wsgi_application()
